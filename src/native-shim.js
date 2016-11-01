7ef9dd43d36378327cb5c61e95f72bc01ff7a7a9
Justin Fagnani <justinfagnani@google.com>
2016-11-01 10:32:38 -0700

    New shim w/o Reflect.construct

--- src/native-shim.js
+++ src/native-shim.js
@@ -18,15 +18,125 @@
  * shim must only be loaded in browsers that do.
  */
 (() => {
-  let origHTMLElement = HTMLElement;
-  // TODO(justinfagnani): Tests!!
+  'use strict';
+
+  const NativeHTMLElement = HTMLElement;
+  const nativeDefine = window.customElements.define;
+  const nativeGet = window.customElements.get;
+
+  /**
+   * Map of user-provided constructors to tag names.
+   *
+   * @type {Map<Function, string>}
+   */
+  const tagnameByConstructor = new Map();
+
+  /**
+   * Map of tag anmes to user-provided constructors.
+   *
+   * @type {Map<string, Function>}
+   */
+  const constructorByTagname = new Map();
+
+
+  // /**
+  //  * Whether the constructors are being called by a browser process, ie parsing
+  //  * or createElement.
+  //  */
+  // let browserConstruction = false;
+  //
+  // /**
+  //  * Whether the constructors are being called by a user-space process, ie
+  //  * calling an element constructor.
+  //  */
+  // let userConstruction = false;
+
   window.HTMLElement = function() {
-    // prefer new.target for elements that call super() constructors or
-    // Reflect.construct directly
-    let newTarget = new.target || this.constructor;
-    return Reflect.construct(origHTMLElement, [], newTarget);
-  }
-  HTMLElement.prototype = Object.create(origHTMLElement.prototype, {
-    constructor: {value: HTMLElement, configurable: true, writable: true},
-  });
+    if (!HTMLElement.browserConstruction) {
+      const tagname = tagnameByConstructor.get(this.constructor);
+      const fakeClass = nativeGet.call(window.customElements, tagname);
+
+      // Make sure that the fake constructor doesn't call back to this constructor
+      HTMLElement.userConstruction = true;
+      const instance = new (fakeClass)();
+      return instance;
+    }
+    // Else do nothing. This will be reached by ES5-style classes doing
+    // HTMLElement.call() during initialization
+    HTMLElement.browserConstruction = false;
+  };
+
+  HTMLElement.prototype = Object.create(NativeHTMLElement.prototype);
+  HTMLElement.prototype.constructor = HTMLElement;
+  HTMLElement.nativeClass = NativeHTMLElement;
+
+
+  /**
+   * Whether the constructors are being called by a browser process, ie parsing
+   * or createElement.
+   */
+  HTMLElement.browserConstruction = false;
+
+  /**
+   * Whether the constructors are being called by a user-space process, ie
+   * calling an element constructor.
+   */
+  HTMLElement.userConstruction = false;
+
+  window.customElements.define = (tagname, elementClass) => {
+
+    const FakeElement = class extends NativeHTMLElement {
+      constructor() {
+        // Call the native HTMLElement constructor, this gives us the
+        // under-construction instance as `this`:
+        super();
+
+        // The prototype will be wrong up because the browser used our fake
+        // class, so fix it:
+        Object.setPrototypeOf(this, elementClass.prototype);
+
+        if (!HTMLElement.userConstruction) {
+          // Make sure that user-defined constructor bottom's out to a do-nothing
+          // HTMLElement() call
+          HTMLElement.browserConstruction = true;
+          // Call the user-defined constructor on our instance:
+          elementClass.call(this);
+        }
+        HTMLElement.userConstruction = false;
+      }
+    }
+    if ('observedAttributes' in elementClass) {
+      FakeElement.observedAttributes = elementClass.observedAttributes;
+    }
+    const elementProto = Object.getPrototypeOf(elementClass);
+    if ('connectedCallback' in elementProto) {
+      FakeElement.prototype.connectedCallback = function() {
+        elementProto.connectedCallback.call(this);
+      }
+    }
+    if ('disconnectedCallback' in elementProto) {
+      FakeElement.prototype.disconnectedCallback = function() {
+        elementProto.disconnectedCallback.call(this);
+      }
+    }
+    if ('attributeChangedCallback' in elementProto) {
+      FakeElement.prototype.attributeChangedCallback = function() {
+        elementProto.attributeChangedCallback.call(this);
+      }
+    }
+    if ('adoptedCallback' in elementProto) {
+      FakeElement.prototype.adoptedCallback = function() {
+        elementProto.adoptedCallback.call(this);
+      }
+    }
+
+    tagnameByConstructor.set(elementClass, tagname);
+    constructorByTagname.set(tagname, elementClass);
+    nativeDefine.call(window.customElements, tagname, FakeElement);
+  };
+
+  window.customElements.get = function(tagname) {
+    return constructorByTagname.get(tagname);
+  };
+
 })();
